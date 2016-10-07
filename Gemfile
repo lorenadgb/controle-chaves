10530c9cee9b604440df3ab8d7c421a48c405002
Lorena Bento <lorenadgb@gmail.com>
2016-10-06 21:11:58 -0300

    Devise: Configuração

--- Gemfile
+++ Gemfile
@@ -1,9 +1,10 @@
 source 'https://rubygems.org'
 
-
 gem 'rails', '4.2.6'
 
 gem 'pg'
+
+gem 'devise', github: 'plataformatec/devise'
 
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module ControleChaves
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.default_locale = 'pt-Br'
+    config.i18n.default_locale = 'pt-BR'
 
     config.encoding = "utf-8"
 
