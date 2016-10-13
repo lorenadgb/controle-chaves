c5e2bb8d8d73c386a98179f7cfa2d54a87e3af14
Lorena Bento <lorenadgb@gmail.com>
2016-10-13 18:59:45 -0300

    i18n: Adicionada traduções

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,38 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [ :show, :edit, :update ]
   load_and_authorize_resource
 
   def index
     @users = User.all
+  end
+
+  def show
+    authorize! :show_for, user
+    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
